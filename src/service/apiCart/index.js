3d8d0766099e98c70183ac0d58c800c1e9904428
VuTuanCuong <114804308+Vucuongtuan@users.noreply.github.com>
2024-05-29 21:08:36 +0700

    Update index.js

--- src/service/apiCart/index.js
+++ src/service/apiCart/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { viewCart, addToCart } = require("../../controller/Cart");
 const {
-  createInvoiceAndSendEmail,
-} = require("../../middleware/invoice/createInvoice");
+  viewCart,
+  addToCart,
+  getAllHoaDon,
+  getHoaDonByUser,
+} = require("../../controller/Cart");
 
 const routerCart = express.Router();
 
 routerCart.get("/", viewCart);
+routerCart.get("/all", getAllHoaDon);
+routerCart.post("/", getHoaDonByUser);
 routerCart.post("/", addToCart);
 module.exports = routerCart;
